5bfc66117b9781e246c44eb4370f82f4cd4cdc5d
PredragTrajkovic3012 <ptrajkovic997@gmail.com>
2021-04-07 13:52:50 +0200

    fix

--- housebudgetapp/app.py
+++ housebudgetapp/app.py
@@ -1,6 +1,7 @@
 from tortoise import Tortoise, fields, run_async
 from tortoise.models import Model
 from models import *
+from tortoise.transactions import in_transaction
 import utils
 import csv
 
@@ -21,10 +22,24 @@ async def init():
     # Generate the schema
     await Tortoise.generate_schemas()
 
-    grupa1 = Group(id_group=utils.generate_uuid4(), name_group="Braca na praksi")
-    losmi = User(id=utils.generate_uuid4(), first_name='Losmi', last_name='Copara', monthly_income=20000,group=grupa1.id)
-    predrag=User(id=utils.generate_uuid4()+"", first="Predragoslav",last_name="Trajkovic",monthly_income=30000,group=grupa1.id)
-    trosak1=Trosak(id_trosak=utils.generate_uuid4(),name="Struja",price=5000,user=losmi.id)
+    grupa1 = Group(name="Braca na praksi")
+    await grupa1.save()
+
+    losmi = User(first_name='Losmi', last_name='Copara', monthly_income=20000, group=grupa1)
+    predrag=User(first_name="Predragoslav",last_name="Trajkovic",monthly_income=30000,group=grupa1)
+
+    async with in_transaction():
+        await losmi.save()
+        await predrag.save()
+
+    
+    await grupa1.fetch_related('users')
+    for user in grupa1.users:
+        print(user)
+
+    
+
+#    trosak1=Trosak(id_trosak=utils.generate_uuid4(),name="Struja",price=5000,user=losmi.id)
 
 
 
@@ -32,7 +47,7 @@ async def init():
     #trosak1 = Trosak()
 
 
-    await losmi.save()
+#    await losmi.save()
     # with open('MOCK_DATA.csv', 'r') as file:
     #     reader = csv.reader(file)
     #     next(reader)
@@ -41,4 +56,6 @@ async def init():
     #                        monthly_income=(float(row[3])))
     #         await predrag.save()
 
-run_async(init())
+if __name__=="__main__":
+    run_async(init())
+
--- housebudgetapp/models.py
+++ housebudgetapp/models.py
@@ -2,43 +2,57 @@ from tortoise import Tortoise, fields
 from tortoise.models import Model
 
 
+class Group(Model):
+    class Meta:
+        table = 'grupe'
+    id = fields.UUIDField(pk=True)
+    name = fields.CharField(max_length=60)
+
+    users: fields.ReverseRelation["User"] = fields.ReverseRelation
+
 class User(Model):
     class Meta:
         table = 'users'
+        
     id = fields.UUIDField(pk = True)
+
     first_name = fields.CharField(max_length=50)
     last_name = fields.CharField(max_length=50, null=True)
     monthly_income = fields.FloatField()
+
+    group = fields.ForeignKeyField('models.Group', index=True, null=False, related_name='users')
+
+    def __str__(self):
+        return f"{self.id} {self.first_name}"
+
     #username mozda pitaj igora
     #pass mozda
 
-    group: fields.ForeignKeyRelation['Group'] = fields.ForeignKeyField(
-        'app.Group', related_name='users', index=True, null=True
-    )
+#    group: fields.ForeignKeyRelation['Group'] = fields.ForeignKeyField(
+#        'models.Group', related_name='users', index=True, null=True
+#    )
 
     # container: fields.ForeignKeyRelation['Container'] = fields.ForeignKeyField(
     #     'waste.Container', related_name='alarms', index=True, null=False, on_delete=fields.CASCADE,
     #     source_field='id_container')
 
 
-
-
 class Trosak(Model):
+
     class Meta:
         table = 'troskovi'
-    id_trosak = fields.UUIDField(pk = True)
+
+    id = fields.UUIDField(pk = True)
     name = fields.CharField(max_length=60)
     price = fields.FloatField()
-    user: fields.ForeignKeyRelation['User'] = fields.ForeignKeyField(
-        'User', related_name='troskovi', index=True, null=False, on_delete=fields.CASCADE
-    )
+
+    user = fields.ForeignKeyField('models.User', index=True)
+
+#    user: fields.ForeignKeyRelation['User'] = fields.ForeignKeyField(
+#        'User', related_name='troskovi', index=True, null=False, on_delete=fields.CASCADE
+#    )
 
 
-class Group(Model):
-    class Meta:
-        table = 'grupe'
-        id_group = fields.UUIDField(pk=True)
-        name_group = fields.CharField(max_length=60)
 
 
 
